158e68dbf5b7d28041a6cf8b9a35822e3b97e8bc
Femi Oladele <profemzy@gmail.com>
2026-01-29 15:13:13 -0800

    Update Azure VM images, provider versions, and SSH keys
    Updated RedHat VM SKU from '83-gen2' to '10-lvm-gen2' in multiple modules. Upgraded Terraform required version and provider versions, added subscription_id to provider blocks, and fixed random_string argument from 'number' to 'numeric'. Refreshed SSH key pairs and made minor cleanups to network and load balancer resources.

--- 16-Azure-SLB-VM-with-MetaArgument-Count/terraform-manifests/terraform.tfvars
+++ 16-Azure-SLB-VM-with-MetaArgument-Count/terraform-manifests/terraform.tfvars
@@ -1,4 +1,4 @@
-business_divsion        = "hr"
+business_divsion        = "pas"
 environment             = "dev"
 resource_group_name     = "rg"
 resource_group_location = "eastus"
--- 17-Azure-SLB-VM-with-for-each-and-for-loops/terraform-manifests/c9-02-web-loadbalancer-resource.tf
+++ 17-Azure-SLB-VM-with-for-each-and-for-loops/terraform-manifests/c9-02-web-loadbalancer-resource.tf
@@ -32,7 +32,6 @@ resource "azurerm_lb_probe" "web_lb_probe" {
   protocol            = "Tcp"
   port                = 80
   loadbalancer_id     = azurerm_lb.web_lb.id
-  resource_group_name = azurerm_resource_group.rg.name
 }
 
 # Resource-5: Create LB Rule
@@ -42,10 +41,9 @@ resource "azurerm_lb_rule" "web_lb_rule_app1" {
   frontend_port                  = 80
   backend_port                   = 80
   frontend_ip_configuration_name = azurerm_lb.web_lb.frontend_ip_configuration[0].name
-  backend_address_pool_id        = azurerm_lb_backend_address_pool.web_lb_backend_address_pool.id
+  backend_address_pool_ids        = [azurerm_lb_backend_address_pool.web_lb_backend_address_pool.id]
   probe_id                       = azurerm_lb_probe.web_lb_probe.id
   loadbalancer_id                = azurerm_lb.web_lb.id
-  resource_group_name            = azurerm_resource_group.rg.name
 }
 
 
